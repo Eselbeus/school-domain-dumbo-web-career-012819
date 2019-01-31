ef35e058e601e8ed1af49d3b2fff323ef8ad5050
eselbeus <eselbeus@gmail.com>
2019-01-31 03:16:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+require 'pry'
+class School
+  attr_accessor :add_student, :roster
+  
+  def initialize(name)
+    @name = name
+    roster = {}
+    @roster = roster
+  end
+  
+  def add_student(student, grade)
+    if @roster[grade]
+      @roster[grade] << student
+      # binding.pry
+    else
+      @roster[grade] = [student]
+    end
+  end
+  
+end
